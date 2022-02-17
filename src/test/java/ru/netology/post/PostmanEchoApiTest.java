971321511b692ace800998795952e80ad918422a
Andrey Martyn <martynandrey@rambler.ru>
2022-02-17 21:10:53 +0600

    Added test for body by post request

---
+++ src/test/java/ru/netology/post/PostmanEchoApiTest.java
@@ -0,0 +1,22 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoApiTest {
+    @Test
+    void shouldReturnPostBody() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello world")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello world"));
+    }
+}
